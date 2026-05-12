20f58ed6844be0b18c8c7593603081bb80018225
Anton Khleb <khleb.4nton@yandex.ru>
2026-05-12 20:00:40 +0500

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
+// Инициализация Swiper
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+        nextEl: '.custom-next',
+        prevEl: '.custom-prev',
+    },
+});
+
 
